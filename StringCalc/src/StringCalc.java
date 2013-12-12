a61f1af0f54cb5f03053e12c6d3816c8a98c760c
taliaco <talicohen88@gmail.com>
2013-12-12 11:54:17 +0200

    refactor add tow numbers

--- StringCalc/src/StringCalc.java
+++ StringCalc/src/StringCalc.java
@@ -5,14 +5,16 @@ public class StringCalc {
 		// TODO Auto-generated method stub
 		
 		String[] numbers = string.split(",");
+		
 		if(string.isEmpty())
 			return 0;
 		else if (string.length()==1){
 			return Integer.parseInt(string);
-			
 		}
 		else{
-			return (Integer.parseInt(numbers[0]) +  Integer.parseInt(numbers[1]));
+			int num1 = Integer.parseInt(numbers[0]);
+			int num2 = Integer.parseInt(numbers[1]);
+			return (num1+num2);
 		}
 	}
 
