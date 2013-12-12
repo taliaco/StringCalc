8553b4d19cc6157f7b62777047615b23338f1e99
taliaco <talicohen88@gmail.com>
2013-12-12 11:44:57 +0200

    refactor add one number

--- StringCalc/src/StringCalc.java
+++ StringCalc/src/StringCalc.java
@@ -3,9 +3,13 @@ public class StringCalc {
 
 	public int Add(String string) {
 		// TODO Auto-generated method stub
+		int num;
 		if(string.isEmpty())
 			return 0;
-		return Integer.parseInt(string);
+		else{
+			num = Integer.parseInt(string);
+		}
+		return num;
 	}
 
 }
